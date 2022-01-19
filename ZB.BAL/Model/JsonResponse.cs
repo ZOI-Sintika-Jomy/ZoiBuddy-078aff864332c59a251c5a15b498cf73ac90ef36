72350d3731198ea769f9ef528685399af1c93a40
Jomy <sinthika.jomy@zoiftspl.com>
2022-01-19 12:31:06 -0500

    Add project files.

---
+++ ZB.BAL/Model/JsonResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZB.BAL.Model
+{
+    public class JsonResponse
+    {
+
+        public long? ID { get; set; }
+
+        public string Status { get; set; }
+        
+        public string Message { get; set; }
+
+        public object PrimaryData { get; set; }
+
+        public object SecondaryData { get; set; }
+        
+        public object TeritaryData { get; set; }
+
+        public string Token { get; set; }
+
+        public string InceptionDate { get; set; }
+
+
+    }
+}
---
+++ ZoiBuddy/wwwroot/js/utilities.js
@@ -0,0 +1,68 @@
+﻿function fn_postAjax(u, d, sh, eh) {
+    if (typeof (showLoadingBox) == "function")
+        showLoadingBox();
+    $.ajax({
+        type: "POST",
+        url: u,
+        data: d,
+        cache: false,
+        success: function (response, status, xhr) {
+            if (typeof (hideLoadingBox) == "function")
+                hideLoadingBox();
+            var ct1 = xhr.getResponseHeader("content-type") || "";
+            var ct = '';
+            if (ct1.indexOf('html') > -1) {
+                ct = 'html';
+            }
+            else if (ct1.indexOf('json') > -1) {
+                ct = 'json';
+            }
+            if (sh)
+                sh(response, ct);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            if (eh)
+                eh(xhr, ajaxOptions, thrownError);
+        }
+    });
+}
+
+function fn_getAjax(u, d, sh, eh) {
+    if (typeof (showLoadingBox) == "function")
+        showLoadingBox();
+    $.ajax({
+        type: "GET",
+        url: u,
+        data: d,
+        success: function (response, status, xhr) {
+            if (typeof (hideLoadingBox) == "function")
+                hideLoadingBox();
+            var ct1 = xhr.getResponseHeader("content-type") || "";
+            var ct = '';
+            if (ct1.indexOf('html') > -1) {
+                ct = 'html';
+            }
+            else if (ct1.indexOf('json') > -1) {
+                ct = 'json';
+            }
+            if (sh)
+                sh(response, ct);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            if (eh)
+                eh(xhr, ajaxOptions, thrownError);
+        }
+    });
+}
+
+function fn_IsJsonArrayEmpty(json_array) {
+    if (json_array != null && json_array.length > 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function fn_Reset() {
+
+}
